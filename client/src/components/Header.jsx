ef1e4b318607f91de5e115873fbe6fb97a543d19
Samantha Allen <samanthaamiraallen@gmail.com>
2024-04-04 10:21:02 -0300

    created basic header using react bootstrap, created external style folder

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,22 @@
-function Header() {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-};
+function Header() {
+  return (
+    <Navbar expand="lg">
+      <Container className='navBar'>
+        <Navbar.Brand href="home">Custom Festival Wear</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link href="home" className='navLink'>Home</Nav.Link>
+            <Nav.Link href="portfolio" className='navLink'>Portfolio</Nav.Link>
+            <Nav.Link href="design" className='navLink'>Design</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Header;
