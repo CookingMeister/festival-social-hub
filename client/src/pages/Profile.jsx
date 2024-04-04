b31054eb86b27d53ec2d0dd99272faec8c5bc8ae
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-04-04 14:23:05 -0300

    update login.jsx, protectedRoute, protectedProfile, Profile.jsx, App2.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,16 @@
-function Profile() {
-    return (
-        <div>
-            <h1>Profile</h1>
-        </div>
-    );
+import PropTypes from 'prop-types';
+
+function Profile({ welcome }) {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <h2>Welcome {welcome}</h2>
+    </div>
+  );
 }
+
+Profile.propTypes = {
+  welcome: PropTypes.string.isRequired,
+};
 
 export default Profile;
