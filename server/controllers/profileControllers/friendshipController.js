ef8d1b80e7d277abbdf173640626dd617901873b
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-04 14:52:54 -0300

    fixed errors in controllers

---
+++ server/controllers/profileControllers/friendshipController.js
@@ -0,0 +1,20 @@
+// friendshipController.js
+import Friendship from '../models/profileModels/friendshipModel/friendship';
+
+export const createFriendship = async (req, res) => {
+  try {
+    const friendship = await Friendship.create(req.body);
+    res.status(201).json(friendship);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getFriendships = async (req, res) => {
+  try {
+    const friendships = await Friendship.find();
+    res.status(200).json(friendships);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
