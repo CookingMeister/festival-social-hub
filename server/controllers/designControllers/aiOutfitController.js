3e9e13f588d214930cfdb2d2e3e85e54f1251c8e
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-04 12:06:18 -0300

    reorganized controllers

---
+++ server/controllers/designControllers/aiOutfitController.js
@@ -0,0 +1,20 @@
+// aiOutfitController.js
+import AIOutfit from '../models/aiOutfitModel/aiOutfit';
+
+export const createAIOutfit = async (req, res) => {
+  try {
+    const aiOutfit = await AIOutfit.create(req.body);
+    res.status(201).json(aiOutfit);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getAIOutfits = async (req, res) => {
+  try {
+    const aiOutfits = await AIOutfit.find();
+    res.status(200).json(aiOutfits);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ server/controllers/profileContollers/playlistController.js
@@ -0,0 +1,20 @@
+// playlistController.js
+import Playlist from '../models/playlist';
+
+export const createPlaylist = async (req, res) => {
+  try {
+    const playlist = await Playlist.create(req.body);
+    res.status(201).json(playlist);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getPlaylists = async (req, res) => {
+  try {
+    const playlists = await Playlist.find();
+    res.status(200).json(playlists);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
