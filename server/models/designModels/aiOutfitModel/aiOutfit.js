f913200742bf0e4cf2f5fc8daf0b2d4750dbf843
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-04-04 09:54:42 -0300

    added models with corrected format

---
+++ server/models/designModels/aiOutfitModel/aiOutfit.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const aiOutfitSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  image: { type: String, required: true }, // URL of the rendered outfit image
+  description: { type: String }, // Further description provided by the user
+  visibility: { type: String, enum: ['public', 'private'], default: 'public' }
+});
+
+const AIOutfit = mongoose.model('AIOutfit', aiOutfitSchema);
+
+export default AIOutfit;
---
+++ server/models/profileModels/commentModel/comment.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
