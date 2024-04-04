1756521586d8f06de4502f7bc9e7d8ad29040edb
Samantha Allen <samanthaamiraallen@gmail.com>
2024-04-04 19:01:29 -0300

    added logo to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <Row>
+        <Col xs={4}>
+            <Image src="/public/logo.png" roundedCircle />
+        </Col>
+        <Col xs={8}>2 of 3</Col>
+      </Row>
+      <Row>
+        <Col xs={8}>1 of 3</Col>
+        <Col xs={4}>2 of 3</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Home;
