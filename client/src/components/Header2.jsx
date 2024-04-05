22cc0eb2746ebdf6c52de3fd5fc13e29cdd4fd73
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-04-05 16:17:34 -0300

    add register, login logout functionality, conditional rendering of navlinks depending on loggedIn or not, working mostly

--- client/src/components/Header2.jsx
+++ client/src/components/Header2.jsx
@@ -1,8 +1,21 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import LogoutButton from './LogoutButton';
 
 function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem('token');
+      token ? setIsLoggedIn(true) : setIsLoggedIn(false);
+    };
+
+    checkLoginStatus();
+  }, []);
+
   return (
     <div>
       <Navbar expand="lg" className="navBar">
@@ -22,41 +35,50 @@ function Header() {
           >
             Home
           </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/register"
-            style={{ color: '#FFFB0A', padding: '5px' }}
-          >
-            Register
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/login"
-            style={{ color: '#FFFB0A', padding: '5px' }}
-          >
-            Login
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/profile"
-            style={{ color: '#FFFB0A', padding: '5px' }}
-          >
-            Profile
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/design"
-            style={{ color: '#FFFB0A', padding: '5px' }}
-          >
-            Design
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="/artmarket"
-            style={{ color: '#FFFB0A', padding: '5px' }}
-          >
-            Art Market
-          </Nav.Link>
+          {!isLoggedIn && (
+            <>
+              <Nav.Link
+                as={Link}
+                to="/register"
+                style={{ color: '#FFFB0A', padding: '5px' }}
+              >
+                Register
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/login"
+                style={{ color: '#FFFB0A', padding: '5px' }}
+              >
+                Login
+              </Nav.Link>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <Nav.Link
+                as={Link}
+                to="/profile"
+                style={{ color: '#FFFB0A', padding: '5px' }}
+              >
+                Profile
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/design"
+                style={{ color: '#FFFB0A', padding: '5px' }}
+              >
+                Design
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/artmarket"
+                style={{ color: '#FFFB0A', padding: '5px' }}
+              >
+                Art Market
+              </Nav.Link>
+              <LogoutButton setIsLoggedIn={setIsLoggedIn} />
+            </>
+          )}
         </Nav>
       </Navbar>
     </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
 
 function Profile({ welcome }) {
   return (
     <div>
-      <h1>Profile</h1>
-      <h2>Welcome {welcome}</h2>
+      <h1 className="text-center">Profile</h1>
+      <h3 className="text-center">
+        {welcome ? `Welcome ${welcome}` : 'Welcome Thirsty Traveller'}
+      </h3>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          eveniet! Optio eum suscipit quos dolorem perferendis itaque adipisci
+          fuga ducimus sapiente assumenda? Eum assumenda nostrum accusantium
+          veritatis sit odit cupiditate.
+        </p>
+      </Container>
     </div>
   );
 }
 
 Profile.propTypes = {
-  welcome: PropTypes.string.isRequired,
+  welcome: PropTypes.string,
 };
 
 export default Profile;
