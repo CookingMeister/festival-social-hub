39f91db03f94788bfbf8121094f1602f0aae9714
Samantha Allen <samanthaamiraallen@gmail.com>
2024-04-05 15:05:57 -0300

    added use state to ar market so that selected items appear on dropdown button, changed hightlight over drop down items

--- client/src/pages/ArtMarket.jsx
+++ client/src/pages/ArtMarket.jsx
@@ -1,7 +1,25 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import products from '../utils/products.json'
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function ArtMarket() {
+    const [selectedSize, setSelectedSize] = useState(null);
+    const [selectedStyle, setSelectedStyle] = useState(null);
+    const [selectedBestSeller, setSelectedBestSeller] = useState(null);
+
+    const handleSizeSelect = (size) => {
+        setSelectedSize(size);
+    };
+
+    const handleStyleSelect = (style) => {
+        setSelectedStyle(style);
+    };
+
+    const handleBestSellerSelect = (bestSeller) => {
+        setSelectedBestSeller(bestSeller);
+    };
+
 
     return (
         <div className='mx-3'>
@@ -9,37 +27,45 @@ function ArtMarket() {
             <div className='d-flex justify-content-center'>
                 <Dropdown className='mx-2'>
                     <Dropdown.Toggle id="dropdown-basic" style={{ backgroundColor: "#5F6695", outline: '#5F6695', color: '#FFFB0A' }}>
-                        Size
+                    {selectedSize ? selectedSize : 'Size'}
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu style={{ backgroundColor: "#5F6695" }}>
                         {products.sizes.map((size, index) => (
-                                <Dropdown.Item style={{ color: '#FFFB0A' }}  key={index}>{size}</Dropdown.Item>
+                            <Dropdown.Item style={{ color: '#FFFB0A' }} key={index} onClick={() => handleSizeSelect(size)}>{size}</Dropdown.Item>
                         ))}
                     </Dropdown.Menu>
                 </Dropdown>
                 <Dropdown className='mx-2'>
                     <Dropdown.Toggle id="dropdown-basic" style={{ backgroundColor: "#5F6695", outline: '#5F6695', color: '#FFFB0A' }}>
-                        Style
+                    {selectedStyle ? selectedStyle : 'Style'}
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu style={{ backgroundColor: "#5F6695" }}>
                         {products.styles.map((style, index) => (
-                            <Dropdown.Item style={{ color: '#FFFB0A' }} key={index}>{style}</Dropdown.Item>
+                            <Dropdown.Item style={{ color: '#FFFB0A' }} key={index} onClick={() => handleStyleSelect(style)}>{style}</Dropdown.Item>
                         ))}
                     </Dropdown.Menu>
                 </Dropdown>
                 <Dropdown className='mx-2'>
                     <Dropdown.Toggle id="dropdown-basic" style={{ backgroundColor: "#5F6695", outline: '#5F6695', color: '#FFFB0A' }}>
-                        Best Seller
+                    {selectedBestSeller ? selectedBestSeller : 'Best Seller'}
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu style={{ backgroundColor: "#5F6695" }}>
                         {products.BestSellers.map((bestSeller, index) => (
-                            <Dropdown.Item style={{ color: '#FFFB0A' }}  key={index}>{bestSeller}</Dropdown.Item>
+                            <Dropdown.Item style={{ color: '#FFFB0A' }} key={index} onClick={() => handleBestSellerSelect(bestSeller)}>{bestSeller}</Dropdown.Item>
                         ))}
                     </Dropdown.Menu>
                 </Dropdown>
+            </div>
+            <div className='mt-2 d-flex justify-content-center'>
+                <Button className='m-1' style={{ backgroundColor: "#5F6695", outline: '#5F6695', color: '#FFFB0A'}}>
+                    Add to Cart
+                </Button>
+                <Button className='m-1' style={{ backgroundColor: "#5F6695", outline: '#5F6695', color: '#FFFB0A'}}>
+                    Go to Cart
+                </Button>
             </div>
         </div>
     );
