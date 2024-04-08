5f5f0d24fee77fa3e6c8d26cb0deaa102b49a238
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-04-08 12:25:31 -0300

    add styling to real carousel images, not placeholders

--- client/src/components/CarouselImage.jsx
+++ client/src/components/CarouselImage.jsx
@@ -5,27 +5,48 @@ import FlowerCrown from '../components/FlowerCrown';
 
 function CarouselImage() {
   return (
-    <Carousel fluid='true'>
-      <Carousel.Item interval={2000}>
-        <MermaidCrown text='Mermaid Crown' />
+    <Carousel
+      // fluid="true"
+      style={{
+        minHeight: '380px',
+        width: '100%',
+        overflow: 'hidden',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '2% 2% 0',
+        marginBottom: '5%',
+      }}
+    >
+      <Carousel.Item interval={4000}>
+        <MermaidCrown
+          text="Mermaid Crown"
+        />
         <Carousel.Caption>
-          <h3 style={{ color: "#fffb0a" }}>Mermaid Crown</h3>
-          <p style={{ color: "#fffb0a" }}>Handcrafted crown adorned with shells and pearls.</p>
+          <h3 style={{ color: '#fffb0a' }}>Mermaid Crown</h3>
+          <p style={{ color: '#fffb0a' }}>
+            Handcrafted crown adorned with shells and pearls.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <JeanJacket text='Jean Jacket' />
+      <Carousel.Item interval={4000}>
+        <JeanJacket
+          text="Jean Jacket"
+        />
         <Carousel.Caption>
-          <h3 style={{ color: "#fffb0a" }}>Jean Jacket</h3>
-          <p style={{ color: "#fffb0a" }}>Upcycled denim beauties ready to wear anywhere.</p>
+          <h3 style={{ color: '#fffb0a' }}>Jean Jacket</h3>
+          <p style={{ color: '#fffb0a' }}>
+            Upcycled denim beauties ready to wear anywhere.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={2000}>
-        <FlowerCrown text='Flower Crown' />
+      <Carousel.Item interval={4000}>
+        <FlowerCrown
+          text="Flower Crown"
+        />
         <Carousel.Caption>
-          <h3 style={{ color: "#fffb0a" }}>Flower Crown</h3>
-          <p style={{ color: "#fffb0a" }}>
-          Forever flower crown made with assorted blooms.
+          <h3 style={{ color: '#fffb0a' }}>Flower Crown</h3>
+          <p style={{ color: '#fffb0a' }}>
+            Forever flower crown made with assorted blooms.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
--- client/src/components/MermaidCrown.jsx
+++ client/src/components/MermaidCrown.jsx
@@ -1,16 +1,20 @@
 function MermaidCrown() {
 
-    return (
-      <div className="my-3" style={{ width: '80%', marginLeft: '10%' }}>
-        <img
-          className="d-block w-100"
-          style={{ borderRadius: '8px', objectFit: 'cover' }}
-          src="mermaidcrown.jpg"
-          alt="Mermaid Crown"
-        />
-      </div>
-    );
-  }
-  
-  export default MermaidCrown;
-  
+  return (
+    <div className="my-3" style={{ height: '380px', }}>
+      <img
+        className="d-block"
+        style={{
+          borderRadius: '8px',
+          objectFit: 'contain',
+          width: '82%',
+          margin: '0 auto'
+        }}
+        src="mermaidcrown.jpg"
+        alt="Mermaid Crown"
+      />
+    </div>
+  );
+}
+
+export default MermaidCrown;
