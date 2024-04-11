12b572cef99cb32813fb32dd9443451da33709a4
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-04-11 15:48:55 -0300

    add delete user to profilepage and modal popup confirm window, clears localstorage token and redirects to Homepage, working

--- server/routes/api/authRoutes.js
+++ server/routes/api/authRoutes.js
@@ -133,4 +133,19 @@ router.get('/products', authMiddleware, async (req, res) => {
   }
 });
 
+router.delete('/users/profile', authMiddleware, async (req, res) => {
+  try {
+    const userId = req.userId;
+    console.log('userId:', userId);
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    console.log('Deleted User:', user);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 export default router;
