bfef9d1ab0d395950a279f14a9903baff8fcad80
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-04-15 18:15:35 -0300

    refactor backend routes and controllers complete to MVC specs, another delete button styling for review in Admin

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,10 +2,10 @@ import {
   generateToken,
   hashPassword,
   comparePassword,
-  authMiddleware,
 } from './../utils/auth.js';
 import User from './../models/profileModels/userModel/user.js';
 
+//  Register User
 const registerUser = async (req, res) => {
   console.log(req.body);
   try {
@@ -41,51 +41,81 @@ const registerUser = async (req, res) => {
   }
 };
 
-const updateUserProfile = async (req, res) => {
-    try {
-      const { name, username, password, socials, aboutMe, topFestivals } =
-        req.body;
-      const userId = req.userId;
-      console.log('userId:', userId);
-  
-      const updateFields = { name, socials, aboutMe, topFestivals };
-  
-      if (username) {
-        updateFields.username = username;
-      }
-  
-      if (password) {
-        updateFields.password = await hashPassword(password);
-      }
-  
-      // Find the user by ID and update the profile fields
-      const user = await User.findByIdAndUpdate(userId, updateFields, {
-        new: true,
-      });
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-      console.log('Updated User:', user);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+// Get All Users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-password');
+    if (users.length === 0) {
+      return res.status(404).json({ error: 'No users found' });
     }
-  };
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+};
 
-  const createUserProfile = async (req, res) => {
-    try {
-      const { password, ...userData } = req.body;
-      // Hash the password
-      const hashedPassword = await hashPassword(password);
-      const newUser = new User({ ...userData, password: hashedPassword });
-      const savedUser = await newUser.save();
-      console.log('New User:', savedUser);
-      res.status(201).json(savedUser);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+// Get User By ID
+const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    console.log('userId:', userId);
+    const user = await User.findById(userId).select('-password');
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
     }
-  };
+    console.log('User:', user);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch user data' });
+  }
+};
+
+//  Update User Profile
+const updateUserProfile = async (req, res) => {
+  try {
+    const { name, username, password, socials, aboutMe, topFestivals } =
+      req.body;
+    const userId = req.userId;
+    console.log('userId:', userId);
+
+    const updateFields = { name, socials, aboutMe, topFestivals };
+
+    if (username) {
+      updateFields.username = username;
+    }
+
+    if (password) {
+      updateFields.password = await hashPassword(password);
+    }
+
+    // Find the user by ID and update the profile fields
+    const user = await User.findByIdAndUpdate(userId, updateFields, {
+      new: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    console.log('Updated User:', user);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const createUserProfile = async (req, res) => {
+  try {
+    const { password, ...userData } = req.body;
+    // Hash the password
+    const hashedPassword = await hashPassword(password);
+    const newUser = new User({ ...userData, password: hashedPassword });
+    const savedUser = await newUser.save();
+    console.log('New User:', savedUser);
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const loginUser = async (req, res) => {
   try {
@@ -108,6 +138,20 @@ const loginUser = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    console.log('Deleted User:', user);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 const logoutUser = (req, res) => {
   try {
     // Clear the token from the client-side local storage
@@ -118,4 +162,13 @@ const logoutUser = (req, res) => {
   }
 };
 
-export { registerUser, loginUser, updateUserProfile, createUserProfile, logoutUser };
+export {
+  registerUser,
+  getAllUsers,
+  getUserById,
+  loginUser,
+  updateUserProfile,
+  createUserProfile,
+  deleteUser,
+  logoutUser,
+};
